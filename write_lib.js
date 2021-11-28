70f508b475905224b5933a912bc75e4e3081fb1d
copan31 <52942558+copan31@users.noreply.github.com>
2021-11-28 21:38:20 +0900

    modify write_lib

--- write_lib.js
+++ write_lib.js
@@ -17,6 +17,17 @@
     }
 
     var write_lib = function(lib, text) {
+        //  オープンモード
+        var FORREADING = 1; // 読み取り専用
+        var FORWRITING = 2; // 書き込み専用
+        var FORAPPENDING = 8; // 追加書き込み
+
+        //  開くファイルの形式
+        var TRISTATE_TRUE = -1; // Unicode
+        var TRISTATE_FALSE = 0; // ASCII
+        var TRISTATE_USEDEFAULT = -2; // システムデフォルト
+
+
         var lib_path = "D:\\local_repos\\sakura_macro";
 
         var objFS = new ActiveXObject("Scripting.FileSystemObject");
@@ -24,17 +35,15 @@
         objFS.CreateTextFile(module_path);
 
         var objFile = objFS.GetFile(module_path);
-        var objText = objFile.OpenAsTextStream(2)
-
+        var objText = objFile.OpenAsTextStream(FORWRITING)
         objText.Write(text);
-
         objText.Close();
     }
 
 
     var lib = "./lib/write_test.js";
     var text = "var CONFIG = { " +
-        "'DEBUG_MODE': 'test' " +
+        "'DEBUG_MODE': 'test2' " +
         "}";
     write_lib(lib, text);
 }())
